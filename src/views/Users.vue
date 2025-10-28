9b36dba65c54b012bafa63dde16974028512d81a
Drew <dev@novaconnect.kz>
2025-10-28 23:26:04 +0500

    /users  изменил опции пагинации на странице значения: 5, 10, 25, 50, 75, 100, 150

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -779,14 +779,13 @@ const tableHeaders = computed(() => [
 
 // Доступные значения для количества элементов на странице
 const perPageOptions = [
+  { title: '5', value: 5 },
   { title: '10', value: 10 },
-  { title: '20', value: 20 },
+  { title: '25', value: 25 },
   { title: '50', value: 50 },
+  { title: '75', value: 75 },
   { title: '100', value: 100 },
-  { title: '200', value: 200 },
-  { title: '500', value: 500 },
-  { title: '1000', value: 1000 },
-  { title: 'Все', value: -1 },
+  { title: '150', value: 150 },
 ];
 
 // Methods
